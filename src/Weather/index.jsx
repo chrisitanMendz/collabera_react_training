821645b8e0af094b2df0f4961f75f087256274c3
Christian Mendoza <me.christian.mendoza@gmail.com>
2023-02-02 10:54:37 +0800

    Update the reusable try catch

--- src/Weather/index.jsx
+++ src/Weather/index.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent, createRef } from 'react';
+import Fetch from '../global/TryCatchFetch';
 import Clear from './Clear';
 import Form from './Form';
 import Info from './Info';
@@ -6,48 +7,38 @@ import Info from './Info';
 class Weather extends PureComponent {
   state = {
     weatherInfo: null,
+    error: '',
   };
 
   inputRef = createRef();
 
   filterWeather = async e => {
     e.preventDefault();
-
     const input = this.inputRef.current.value;
-
     if (input === '') {
       return;
     }
     const URL = `http://localhost:3000/weatherList?city=${input.charAt(0).toUpperCase() + input.slice(1)}`;
-    const [res, error] = await this.tryCatchFetch(URL);
+    const [res, error] = await Fetch(URL);
     if (error) {
-      throw new Error(error);
-    }
-    if (res.length !== 0) {
-      this.setState({
-        weatherInfo: res[0],
-      });
-      this.inputRef.current.value = '';
+      this.setState({ error, weatherInfo: null });
       return;
     }
-    alert('City does not exist!');
-  };
+    if (res.length === 0) {
+      this.setState({ error: 'No data found!', weatherInfo: null });
+      return;
+    }
 
-  reset = () => {
-    this.setState({ weatherInfo: null });
+    this.setState({
+      weatherInfo: res[0],
+      error: '',
+    });
     this.inputRef.current.value = '';
   };
 
-  tryCatchFetch = async (url, params = {}) => {
-    try {
-      const res = await fetch(url, params);
-      if (res.ok) {
-        return [await res.json(), null];
-      }
-      return [null, await res.json()];
-    } catch (error) {
-      return [null, error];
-    }
+  reset = () => {
+    this.setState({ weatherInfo: null, error: '' });
+    this.inputRef.current.value = '';
   };
 
   render() {
@@ -56,6 +47,7 @@ class Weather extends PureComponent {
         <div className="flex flex-col min-w-[300px]">
           <Form search={this.filterWeather} ref={this.inputRef} />
           {this.state.weatherInfo !== null && <Info info={this.state.weatherInfo} />}
+          {this.state.error && <h1 className="self-center mt-3">{this.state.error}</h1>}
           <Clear reset={this.reset} />
         </div>
       </div>
