1b518a1365f0b8b5d4fdaa82ae8ec207c440d4b4
Christian Mendoza <me.christian.mendoza@gmail.com>
2023-02-02 11:13:42 +0800

    Add loading

--- src/Weather/Clear.jsx
+++ src/Weather/Clear.jsx
@@ -1,11 +1,13 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
-const Clear = ({ reset }) => (
+const Clear = props => (
   <button
-    onClick={reset}
-    className="text-xs font-semibold bg-red-400 text-white px-5 py-2 mt-8 rounded-md text-center w-fit duration-300 self-center shadow-md
-      hover:opacity-75"
+    onClick={props.reset}
+    className={`text-xs font-semibold bg-red-400 text-white px-5 py-2 mt-8 rounded-md text-center w-fit duration-300 self-center shadow-md
+      hover:opacity-75
+      ${props.isLoading && 'opacity-25 pointer-events-none'}
+    `}
   >
     Clear
   </button>
@@ -13,6 +15,7 @@ const Clear = ({ reset }) => (
 
 Clear.propTypes = {
   reset: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
 
 export default memo(Clear);
--- src/Weather/Form.jsx
+++ src/Weather/Form.jsx
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 
 const Form = forwardRef((props, ref) => (
   <form onSubmit={props.search} className="flex items-center">
-    <input type="text" placeholder="Weather..." ref={ref} className="h-8 bg-white rounded-l-md text-xs flex-1" />
+    <input
+      type="text"
+      placeholder="Weather..."
+      ref={ref}
+      className={`h-8 bg-white rounded-l-md text-xs flex-1 ${props.isLoading && 'opacity-25'}`}
+      disabled={props.isLoading}
+    />
     <button
       type="submit"
-      className="h-8 text-xs font-semibold px-5 rounded-r-md bg-blue-700 text-white duration-300
-        hover:opacity-75"
+      className={`h-8 text-xs font-semibold px-5 rounded-r-md bg-blue-700 text-white duration-300
+        hover:opacity-75
+        ${props.isLoading && 'opacity-25 pointer-events-none'}
+      `}
+      disabled={props.isLoading}
     >
       Search
     </button>
@@ -16,6 +25,7 @@ const Form = forwardRef((props, ref) => (
 
 Form.propTypes = {
   search: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
 
 export default memo(Form);
